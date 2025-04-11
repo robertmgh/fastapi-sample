6b660bfdd278dba31a367c07f21518cf15ccca00
Robert Michnowski <robert.michnowski@onet.eu>
2025-04-11 15:11:33 +0200

    add validaiton

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
+from typing import Annotated
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from pydantic import AfterValidator, BaseModel
 
 app = FastAPI()
 
 class Item(BaseModel):
     text: str
     is_done: bool = False
+    
+def check_text(item: Item):
+    if len(item.text) < 5:
+        raise ValueError(f'Text is to short: {item.text.count()}')
+    return item
 
 items = []
 
@@ -14,9 +20,13 @@ async def root():
     return items
 
 @app.post("/items")
-def create_item(item: Item):
+def create_item(item: Annotated[Item, AfterValidator(check_text)]):
     items.append(item)
     return items
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.model_dump()}
 
 @app.get("/items/{item_id}", response_model=Item)
 async def get_item(item_id: int) -> Item:
