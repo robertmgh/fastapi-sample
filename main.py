14cd5a1b6cf2569490ef7b2c1da4835289082652
Robert Michnowski <robert.michnowski@onet.eu>
2025-04-17 00:54:21 +0200

    format file

--- main.py
+++ main.py
@@ -3,48 +3,60 @@ from fastapi import Body, Cookie, FastAPI, HTTPException, Request
 from fastapi.responses import JSONResponse
 from pydantic import AfterValidator, BaseModel, Field
 
+
 class ValidationCustomException(Exception):
     def __init__(self, name: str):
         self.name = name
 
+
 app = FastAPI()
+
 
 @app.exception_handler(ValidationCustomException)
 async def test_exception_handler(request: Request, exc: ValidationCustomException):
     return JSONResponse(
-        status_code=400,
-        content={"message": f"Validation error: {exc.name}"}
+        status_code=400, content={"message": f"Validation error: {exc.name}"}
     )
+
 
 class Item(BaseModel):
     text: str
     is_done: bool = False
     price: float = Field(gt=0, description="The price must be greater then zero")
     tags: List[str]
-    
+
+
 def check_text(item: Item):
     if len(item.text) < 5:
-        raise ValueError(f'Text is to short: {len(item.text)}')
+        raise ValueError(f"Text is to short: {len(item.text)}")
     return item
 
+
 items = []
+
 
 @app.get("/items")
 async def root():
     return items
 
+
 @app.post("/items", status_code=201)
-def create_item(test: Annotated[str | None, Cookie()], item: Annotated[Item, AfterValidator(check_text), Body(example=[{
-    "text": "test doc"
-}])]):
+def create_item(
+    test: Annotated[str | None, Cookie()],
+    item: Annotated[
+        Item, AfterValidator(check_text), Body(example=[{"text": "test doc"}])
+    ],
+):
     if item.is_done == False:
         raise ValidationCustomException("Item is not done")
     items.append(item)
     return {"test": test, **item.model_dump()}
 
+
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     return {"item_id": item_id, **item.model_dump()}
+
 
 @app.get("/items/{item_id}", response_model=Item)
 async def get_item(item_id: int) -> Item:
